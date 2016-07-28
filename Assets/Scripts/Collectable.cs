3c7897f1a08fca1164a614063b9bad7c2213a1e8
Justin Lim <justlim77@gmail.com>
2016-07-28 18:38:00 +0800

    Boost Fix | Collectable Implementation | Magnet Feature

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Collectable : MonoBehaviour, IInteractable
+{
+    public float magnetSpeed = 5.0f;
+    public float shrinkSpeed = 1.0f;
+    public float destroyTime = 3.0f;
+
+    Transform _Target;
+
+    bool _HasInteracted = false;
+    public void Interact(object sender, object arg)
+    {
+        if (_HasInteracted)
+            return;
+
+        if (arg is Transform)
+        {
+            _HasInteracted = true;
+
+             _Target = (Transform)arg;
+            Destroy(gameObject, destroyTime);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (_Target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _Target.position, magnetSpeed * Time.deltaTime);
+
+            transform.localScale = Vector3.MoveTowards(transform.localScale, Vector3.zero, shrinkSpeed * Time.deltaTime);
+        }
+	}
+}
--- Assets/scripts/Bird.cs
+++ Assets/scripts/Bird.cs
@@ -39,18 +39,39 @@ public class Bird : MonoBehaviour
     public float BoostMultiplier = 2.0f;
     public KeyCode BoostKey = KeyCode.Space;
     public ParticleSystem BoostParticles;
+    public float BoostRadius = 5.0f;
 
-    [SerializeField] Animator _Animator;
-
-    public float Distance
+    [Header("Magnet")]
+    [SerializeField] Transform _MagnetPoint;
+    Transform MagnetPoint
     {
         get
         {
-            return transform.position.z;
+            if (_MagnetPoint == null)
+            {
+                Debug.LogWarning("No Magnet Point found, returning transform");
+                return transform;
+            }
+            return _MagnetPoint;
         }
     }
 
+
+    public float Distance
+    {
+        get { return transform.position.z; }
+    }
+
+    SphereCollider _Collider;
+    float _OriginalRadius;
+    public float Radius
+    {
+        get { return _Collider.radius; }
+        set { _Collider.radius = value; }
+    }
+
     float _GroundHeight = 0.5f;
+    Animator _Animator;
     Vector3 _Force;
     Vector3 _OriginalPosition;
     Rigidbody _Rigidbody;
@@ -60,7 +81,10 @@ public class Bird : MonoBehaviour
     {
         _Animator = GetComponentInChildren<Animator>();
         _Rigidbody = GetComponent<Rigidbody>();
+        _Collider = GetComponent<SphereCollider>();
+
         _OriginalPosition = transform.position;
+        _OriginalRadius = _Collider.radius;
     }
 
 	// Use this for initialization
@@ -141,13 +165,13 @@ public class Bird : MonoBehaviour
                 float x = MathF.ConvertRange(0, 1, HorizontalMin, HorizontalMax, flightScreenPosition.x);
                 float y = MathF.ConvertRange(0, 1, VerticalMin, VerticalMax, flightScreenPosition.y);
 
-                GuidedForce = new Vector3(x, y, 0);
+                GuidedForce = new Vector3(x, y, _Rigidbody.position.z);
             }
 
+            Vector3 targetPos = Vector3.Lerp(_Rigidbody.position, GuidedForce, 3 * Time.deltaTime);
+            _Rigidbody.position = targetPos;
+
             _Rigidbody.MovePosition(_Rigidbody.position + _Force * Time.deltaTime);
-            GuidedForce.z = _Rigidbody.position.z;
-            Vector3 targetPos = Vector3.Lerp(transform.position, GuidedForce, 3 * Time.deltaTime);
-            transform.position = targetPos;
         }
     }
 
@@ -161,13 +185,13 @@ public class Bird : MonoBehaviour
 
     void Boost()
     {
-        if (forceApplied == true)
-            return;
-
         forceApplied = true;
-        BoostParticles.Play();
-        _Force *= BoostMultiplier;
-        ArduinoUI.Instance.UpdateMessage("BOOSTING!");
+
+        SetAnimation(AnimationState.Glide); // Change to glide animation
+        Radius = BoostRadius;               // Increase collection radius
+        BoostParticles.Play();              // Play boost particles
+        _Force *= BoostMultiplier;          // Multiply forward force
+        ArduinoUI.Instance.UpdateMessage("BOOSTING!");  // Update message
 
         Invoke("CancelBoost", BoostTime);
     }
@@ -175,9 +199,12 @@ public class Bird : MonoBehaviour
     void CancelBoost()
     {
         forceApplied = false;
-        BoostParticles.Stop();
-        _Force = MovementForce;
-        ArduinoUI.Instance.UpdateMessage("");
+
+        SetAnimation(AnimationState.Fly);   // Change to fly animation
+        Radius = _OriginalRadius;           // Revert collection radius
+        BoostParticles.Stop();              // Stop boost particles
+        _Force = MovementForce;             // Revert forward force
+        ArduinoUI.Instance.UpdateMessage("");   // Update message
     }
 
     IEnumerator TakeOff()
@@ -214,5 +241,14 @@ public class Bird : MonoBehaviour
     {
         string trigger = state.ToString().ToLower();
         _Animator.SetTrigger(trigger);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        IInteractable interactable = other.GetComponent<IInteractable>();
+        if (interactable != null)
+        {
+            interactable.Interact(this, MagnetPoint);
+        }
     }
 }
